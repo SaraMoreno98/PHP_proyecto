119d3749ca46e2496f78a55bf9c33e5b9f015eb0
Sara Moreno <saramo.graphic@gmail.com>
2024-10-15 12:53:45 +0200

    Commit validator arreglar

--- php/data/validator.php
+++ php/data/validator.php
@@ -9,25 +9,29 @@ class Validator{
         return $saneados;
     }
 
-    //validar nombre
-    if(!isset($data['nombre']) || empty(trim($data['nombre']))){
-        $errors['nombre'] = "El nombre es necesario";
-    }elseif(strlen($data['nombre']) < 2 || strlen($data['nombre']) > 15){
-        $errors['nombre'] = "El nombre debe tener entre 2 y 15 caracteres";
+    public static function validarDirector($data){
+        $errors = [];
+
+        //validar nombre
+        if(!isset($data['nombre']) || empty(trim($data['nombre']))){
+            $errors['nombre'] = "El nombre es necesario";
+        }elseif(strlen($data['nombre']) < 2 || strlen($data['nombre']) > 15){
+            $errors['nombre'] = "El nombre debe tener entre 2 y 15 caracteres";
+        }
+
+        //validar apellido
+        if(!isset($data['apellido']) || empty(trim($data['apellido']))){
+            $errors['apellido'] = "El apellido es necesario";
+        }elseif(strlen($data['apellido']) < 2 || strlen($data['apellido']) > 30){
+            $errors['apellido'] = "El apellido debe tener entre 2 y 30 caracteres";
+        }
+
+        //validar biografia
+        if(isset($data['biografia']) && strlen($data['biografia']) > 200){
+            $errors['biografia'] = "La biografia es demasiado extensa";
+        }
+
+        return $errors;
     }
-
-    //validar apellido
-    if(!isset($data['apellido']) || empty(trim($data['apellido']))){
-        $errors['apellido'] = "El apellido es necesario";
-    }elseif(strlen($data['apellido']) < 2 || strlen($data['apellido']) > 30){
-        $errors['apellido'] = "El apellido debe tener entre 2 y 30 caracteres";
-    }
-
-
-    //validar biografia
-    if(isset($data['biografia']) && strlen($data['biografia']) > 200){
-        $errors['biografia'] = "La biografia es demasiado extensa";
-    }
-
-    return $errors;
+    
 }
