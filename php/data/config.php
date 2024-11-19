8bd09a5a61caf1e20bad986de7c8a007ffc9efba
Sara Moreno <saramo.graphic@gmail.com>
2024-11-19 10:13:29 +0100

    Update config.php

--- php/data/config.php
+++ php/data/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER','root');
+define('DB_HOST', '');
+define('DB_USER','');
 define('DB_PASS','');
-define('DB_NAME','recetas_singluten');
+define('DB_NAME','');
 
 // Credenciales para el envio de mails
 define('MAIL_HOST', '');
